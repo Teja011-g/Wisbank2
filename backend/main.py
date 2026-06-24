c99c4b60231162065bcdfdf8cbe36c313cad0fa7
Gurram Teja <gurramteja30@gmail.com>
2026-06-24 09:31:12 +0530

    add deployed url cors

--- backend/main.py
+++ backend/main.py
@@ -70,7 +70,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
-        "https://your-frontend-name.onrender.com",
+        "https://wisbank2-0-5.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
