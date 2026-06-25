82e9631255ba61ea4598df0ddd4d509ff2f3d8e9
Gurram Teja <gurramteja30@gmail.com>
2026-06-25 09:29:41 +0530

    configure database for render postgres

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./wisebank.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./wisebank.db")
+
+connect_args = {}
+
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(
@@ -19,7 +25,6 @@ Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
-
     try:
         yield db
     finally:
